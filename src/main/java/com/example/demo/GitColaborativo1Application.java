90340d67c21c6b492ae790672d8b3b82645fd500
axel roman <axelroma590900@gmail.com>
2024-07-04 18:19:34 -0300

    primer

---
+++ src/main/java/com/example/demo/GitColaborativo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitColaborativo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitColaborativo1Application.class, args);
+	}
+
+}
